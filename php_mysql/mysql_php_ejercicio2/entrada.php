6bb9b48acd5ef3aa12566725e74fb56de1990c6a
adrib2727 <1464795@alu.murciaeduca.es>
2020-11-02 15:37:41 +0100

    02/11/20

---
+++ php_mysql/mysql_php_ejercicio2/entrada.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="procesamiento.php" method="post">
+    <h1>Aqui puedes consultar los usuarios de la empresa: </h1>
+    <input type="text" name="codigo" placeholdeer="Introduce el código del usuario">
+    <input type="submit" value="Obtener datos">
+    </form>
+</body>
+</html>
